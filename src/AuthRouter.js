de1301af8de6b2560dc7e8a06fbeacf12105eca5
kkjjss <jinsukk9763@gmail.com>
2021-12-16 16:04:24 +0900

    build for test

--- src/AuthRouter.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-export function AuthRoute({ authenticated, children }) {
-  const auth = authenticated;
-  return auth ? children : <Navigate to="/login" />;
-}
