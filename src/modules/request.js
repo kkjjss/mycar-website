6fa7b8c2ad4661ef96a132f5a9de46f51d110217
kkjjss <jinsukk9763@gmail.com>
2021-12-17 18:14:20 +0900

    https 적용

--- src/modules/request.js
+++ src/modules/request.js
@@ -4,8 +4,8 @@ axios.defaults.baseURL = '3.38.213.148.8333/';
 axios.defaults.withCredentials = true;
 
 const instance = axios.create({
-  baseURL: 'http://3.38.213.148:8333',
-  timeout: 1000, 
+  baseURL: 'https://3.38.213.148:8333',
+  timeout: 1000,
 });
 
 export default instance;
