77f6ffde7339105cefa80a43b2ffda5ed1dd3c6e
kkjjss <jinsukk9763@gmail.com>
2021-12-17 18:33:27 +0900

    build

--- src/modules/request.js
+++ src/modules/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+const https = require('https');
 
 axios.defaults.baseURL = '3.38.213.148.8333/';
 axios.defaults.withCredentials = true;
@@ -6,6 +7,9 @@ axios.defaults.withCredentials = true;
 const instance = axios.create({
   baseURL: 'https://3.38.213.148:8333',
   timeout: 1000,
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
 });
 
 export default instance;
